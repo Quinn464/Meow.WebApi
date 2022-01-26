21489a626f35554ac9062d998dae410bd1698a6e
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-01-25 21:12:34 -0500

    added Reply class

---
+++ Meow.Data/Reply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meow.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int PawstReplyId { get; set; }
+
+        [Key]
+        public string TextPawst { get; set; }
+
+        [Required]
+        public Guid CatOwnerId { get; set; }
+    }
+}
