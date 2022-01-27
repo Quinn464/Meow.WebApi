3cdca77f34c7162c63956fd736e644784a309b09
Grace Casale <gcasale11@gmail.com>
2022-01-26 22:13:58 -0500

    grace added foreignkey

--- Meow.Models/CatmentListItem.cs
+++ Meow.Models/CatmentListItem.cs
@@ -15,6 +15,15 @@ namespace Meow.Models
 
         [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int PawstReplyId { get; set; }
+        public string PawstReplyTitle { get; set; }
     }
 
 }
